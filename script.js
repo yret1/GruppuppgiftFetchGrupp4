22cfa5b791e215ffec19c26a4bf65509e404e428
Simon Gustavsson <simgus1234@gmail.com>
2024-02-09 09:19:59 +0100

    Fixed Fetch

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
-
 //Api query:
 
 const API_KEY = "687ed96588d723d61f7a8b89012440c0"
 const secret = "76aedade9fbbc0e8"
-const API = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&secret=${secret}&tags=dog}&per_page=20&page=1&format=json&nojsoncallback=1`
+const API = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&secret=${secret}&tags=dog&per_page=20&page=1&format=json&nojsoncallback=1`
 
 const media = "photos"
 
@@ -32,8 +31,14 @@ const fetchData = async () => {
             });
         })
 
+        // .then(() => photoRender())
+
         .catch(error => console.log(error))
 
 }
 
 
+
+
+
+fetchData();
